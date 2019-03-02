5c84192a7a37d9d90d21e12a029c9951cc7ebdd4
罗坤 <1498710037@qq.com>
2019-03-02 14:41:00 +0800

    Initial commit

---
+++ app/src/main/java/com/github/react/sextant/learning/FrameView.java
@@ -0,0 +1,16 @@
+package com.github.react.sextant.learning;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * 新建一个继承View类的自定义组件，在构造方法中初始化view的初始坐标
+ * **/
+
+public class FrameView extends View {
+
+    public FrameView(Context context){
+        super(context);
+
+    }
+}
