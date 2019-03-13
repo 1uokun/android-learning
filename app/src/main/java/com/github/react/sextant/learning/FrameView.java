83c7872d952ff51619533fa51b7bf1e3559af735
罗坤 <1498710037@qq.com>
2019-03-13 16:28:28 +0800

    layout

--- app/src/main/java/com/github/react/sextant/learning/FrameView.java
+++ app/src/main/java/com/github/react/sextant/learning/FrameView.java
@@ -1,6 +1,10 @@
 package com.github.react.sextant.learning;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.View;
 
 /**
@@ -9,8 +13,27 @@ import android.view.View;
 
 public class FrameView extends View {
 
-    public FrameView(Context context){
-        super(context);
+    public float bitmapX;
+    public float bitmapY;
 
+    public FrameView(Context context){
+        //constructor 初始坐标
+        super(context);
+        bitmapX = 0;
+        bitmapY = 200;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.my_future_girlfriend);
+
+        canvas.drawBitmap(bitmap,bitmapX,bitmapY,paint);
+
+        if(bitmap.isRecycled()){
+            bitmap.recycle();
+        }
     }
 }
